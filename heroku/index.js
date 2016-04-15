82c9c31d651c0b3d331ea8788271ed6c12b66130
thanhquan1512 <thanhquan1512@gmail.com>
2016-04-15 14:11:39 +0700

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -32,20 +32,19 @@ app.post('/', function (req, res) {
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-      if(text.indexOf("Hello") > -1 || text.indexOf("chào") > -1) {
-      	sendTextMessage(sender, "Chào con");
-      	phase = 1;
-      }
-      else if (phase === 1){
-      	sendTextMessage(sender, "Muốn thầy xem bói không nào?");
-      	phase = 2;
-      }
-      else if((text.indexOf("có") > -1 || text.indexOf("muốn") > -1 || text.indexOf("yes") > -1) && phase === 2) {
-      	sendTextMessage(sender, "Được, con tên gì?");
-      }
-      else {
-       sendTextMessage(sender, "Thầy chưa hiểu ý, con nói lại cho thầy nghe nào.");
-      }
+      	if (text === 'Generic') {
+		sendGenericMessage(sender)
+		continue
+	}
+	else {
+		sendTextMessage(sender, "Thầy chưa hiểu ý, con nói lại cho thầy nghe nào.");
+	}
+	
+	if (event.postback) {
+		text = JSON.stringify(event.postback)
+		sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+		continue
+	}
     }
   }
   res.sendStatus(200);
@@ -69,9 +68,58 @@ function sendTextMessage(sender, text) {
     if (error) {
       console.log('Error sending message: ', error);
     } else if (response.body.error) {
-						console.log('Error: ', response.body.error);
+    	console.log('Error: ', response.body.error);
    }
  });
+}
+
+function sendGenericMessage(sender) {
+	messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First card",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.messenger.com",
+						"title": "web url"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+					"buttons": [{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
 }
 
 app.listen();
