923a7c9639061cab1616a6f55b675d838a96d019
thanhquan1512 <thanhquan1512@gmail.com>
2016-04-15 11:53:02 +0700

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -26,7 +26,7 @@ app.get('/', function (req, res) {
   }
 });
 
-app.post('/', function (req, res) {
+app.post('/webhook/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
